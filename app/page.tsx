1cc92dd5753e122ea167354d7f2b3832341096e6
Rohithegade02 <rohithegade8@gmail.com>
2025-11-29 22:50:21 +0530

    feat: integrate framer-motion for animations and enhance chat layout with new Providers component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
+import Chat from "@/components/common/Chat/Chat";
 import ChatInput from "@/components/common/ChatInput/ChatInput";
 import { Icon } from "@/components/svg/Icon";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
-      <div className="flex flex-col items-center justify-center w-screen  gap-4">
-        <Icon className=" h-auto w-64" />
-        <ChatInput />
-      </div>
-    </div>
+    <Chat />
   );
 }
